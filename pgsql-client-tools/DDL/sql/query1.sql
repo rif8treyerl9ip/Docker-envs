77756e10044d372d21c9773451167ecdea27a382
rif8treyerl9ip <rif8treyerl9ip@users.noreply.github.com>
2024-03-23 20:17:29 +0900

    Refactor: Improve directory structure.

--- pgsql-client-tools/DDL/sql/query1.sql
+++ pgsql-client-tools/DDL/sql/query1.sql
@@ -1,6 +1,5 @@
 CREATE TABLE ManufacturingSite (
     ManufacturingID INT PRIMARY KEY,
     ProductName VARCHAR(100) NOT NULL,
-    ManufacturingDate DATE NOT NULL,
-    Remarks TEXT,
+    ManufacturingDate DATE NOT NULL
 );
--- pgsql-client-tools/DDL/sql/query2.sql
+++ pgsql-client-tools/DDL/sql/query2.sql
@@ -2,4 +2,5 @@ CREATE TABLE ManufacturingSite (
     ManufacturingID INT PRIMARY KEY,
     ProductName VARCHAR(100) NOT NULL,
     ManufacturingDate DATE NOT NULL,
+    Remarks TEXT
 );
