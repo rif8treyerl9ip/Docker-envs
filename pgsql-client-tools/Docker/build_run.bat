436d75449d0ca3964ce8cfb491c9783c01b857e8
rif8treyerl9ip <rif8treyerl9ip@users.noreply.github.com>
2024-03-23 20:15:32 +0900

    Refactor: Improve directory structure.

---
+++ pgsql-client-tools/Docker/build_run.bat
@@ -0,0 +1,7 @@
+docker build --tag pgsql-client-tools -f pgsql-client-tools/Docker\pgsql-client-dockerfile .
+
+docker run ^
+    -e JQ_USER="%JQ_USER%" ^
+    -e JQ_DB="%JQ_DB%" ^
+    -v "%CD%:/workspace" ^
+    --rm -it --name pgsql-client-toolbox pgsql-client-tools
--- pgsql-client-tools/build_run.bat
+++
@@ -1,7 +0,0 @@
-docker build --tag pgsql-client-tools -f pgsql-client-dockerfile .
-
-docker run \
-    -e JQ_USER="%JQ_USER%" ^
-    -e JQ_DB="%JQ_DB%" ^
-    -v "%CD%:/workspace" \
-    --rm -it --name pgsql-client-toolbox pgsql-client-tools
