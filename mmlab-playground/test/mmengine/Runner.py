0bc9e9957b6743f9f37a02d1f1f96ec670374c23
rif8treyerl9ip <rif8treyerl9ip@users.noreply.github.com>
2024-05-04 00:51:19 +0900

    update

--- mmlab-playground/test/mmengine/Runner.py
+++ mmlab-playground/test/mmengine/Runner.py
@@ -3,7 +3,8 @@ from mmengine.config import Config
 
 # !mim download mmdet --config rtmdet_tiny_8xb32-300e_coco --dest .
 
-cfg = Config.fromfile('../all_cfg/rtmdet_tiny_8xb32-300e_coco.py')
+# cfg = Config.fromfile('../all_cfg/rtmdet_tiny_8xb32-300e_coco.py')
+cfg = Config.fromfile('../all_cfg/solov2_r50_fpn_1x_coco.py')
 cfg.work_dir = './work_dir'
 
 def change_cfg():
@@ -18,14 +19,14 @@ def change_cfg():
     cfg.test_dataloader.dataset.ann_file = cfg.data_root + '/annotations/instances_val2017.json'
     cfg.test_evaluator.ann_file = cfg.data_root + '/annotations/instances_val2017.json'
 
-    print("cfg.data_root:{cfg.data_root}")
-    print("cfg.train_dataloader.dataset.data_root:{cfg.train_dataloader.dataset.data_root}")
-    print("cfg.val_dataloader.dataset.data_root:{cfg.val_dataloader.dataset.data_root}")
-    print("cfg.test_dataloader.dataset.data_root:{cfg.test_dataloader.dataset.data_root}")
-    print("cfg.val_dataloader.dataset.ann_file:{cfg.val_dataloader.dataset.ann_file}")
-    print("cfg.val_evaluator.ann_file:{cfg.val_evaluator.ann_file}")
-    print("cfg.test_dataloader.dataset.ann_file:{cfg.test_dataloader.dataset.ann_file}")
-    print("cfg.test_evaluator.ann_file:{cfg.test_evaluator.ann_file}")
+    print(f"cfg.data_root:{cfg.data_root}")
+    print(f"cfg.train_dataloader.dataset.data_root:{cfg.train_dataloader.dataset.data_root}")
+    print(f"cfg.val_dataloader.dataset.data_root:{cfg.val_dataloader.dataset.data_root}")
+    print(f"cfg.test_dataloader.dataset.data_root:{cfg.test_dataloader.dataset.data_root}")
+    print(f"cfg.val_dataloader.dataset.ann_file:{cfg.val_dataloader.dataset.ann_file}")
+    print(f"cfg.val_evaluator.ann_file:{cfg.val_evaluator.ann_file}")
+    print(f"cfg.test_dataloader.dataset.ann_file:{cfg.test_dataloader.dataset.ann_file}")
+    print(f"cfg.test_evaluator.ann_file:{cfg.test_evaluator.ann_file}")
 
 change_cfg()
 
